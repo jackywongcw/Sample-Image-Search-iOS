88c0c8139080e02661897fa0089c7cd4410697f8
JackyWongCW <jackywongcw@outlook.com>
2022-09-18 17:31:22 +0800

    Added response models from API.
    ImageQueryModel - json response model representative of 'value' object from API.
    SearchResponseModel - json response model representative of response from API.

---
+++ ImageSearch/ImageSearch/Model/SearchResponseModel.swift
@@ -0,0 +1,15 @@
+//
+//  SearchResponseModel.swift
+//  ImageSearch
+//
+//  Created by Jacky Wong on 18/9/22.
+//
+
+import Foundation
+
+struct SearchResponseModel: Decodable {
+	let _type: String?
+	let totalCount: Int?
+	
+	let value: [ImageQueryModel]?
+}
