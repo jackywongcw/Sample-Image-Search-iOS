51a984f14c6cc5bbd11ec22a5aa6e36df1dbbb69
JackyWongCW <jackywongcw@outlook.com>
2022-09-20 00:51:59 +0800

    Update of UI Layout for UITableViewCell
    Design inspired from Youtube app

--- ImageSearch/ImageSearch/View/ImageResultTableViewCell.swift
+++ ImageSearch/ImageSearch/View/ImageResultTableViewCell.swift
@@ -9,17 +9,38 @@ import UIKit
 
 class ImageResultTableViewCell: UITableViewCell {
 	
+	@IBOutlet var mainImageView: UIImageView!
+	@IBOutlet var thumbnailImageView: UIImageView!
+	
 	@IBOutlet var titleLabel: UILabel!
+	@IBOutlet var providerTitleLabel: UILabel!
+	@IBOutlet var providerValueLabel: UILabel!
+	@IBOutlet var webpageTitleLabel: UILabel!
+	@IBOutlet var webpageValueLabel: UILabel!
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+		setupUI()
     }
 
+	func setupUI() {
+		titleLabel.numberOfLines = 0
+		providerValueLabel.numberOfLines = 0
+		webpageValueLabel.numberOfLines = 0
+	}
+	
+	func setupData(model: ImageQueryModel) {
+		titleLabel.text = model.title
+		
+		providerTitleLabel.text = "Provider:"
+		providerValueLabel.text = model.provider?.name
+		
+		webpageTitleLabel.text = "Webpage URL:"
+		webpageValueLabel.text = model.webpageUrl
+	}
+	
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
     
 }
